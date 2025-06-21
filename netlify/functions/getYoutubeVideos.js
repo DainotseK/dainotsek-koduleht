3a8b8d4c9e4b56de34956ac2368c4d55c2393a79
DainotseK <dainotsek@gmail.com>
2025-06-21 22:35:31 +0300

    Update getYoutubeVideos.js

--- netlify/functions/getYoutubeVideos.js
+++ netlify/functions/getYoutubeVideos.js
@@ -2,11 +2,11 @@ exports.handler = async function(event, context) {
     const API_KEY = process.env.YOUTUBE_API_KEY;
     const CHANNEL_ID = 'UCFiwaH41voe3jXqAZ2aqP1A';
 
-    const LATEST_VIDEOS_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=15&type=video`;
-    const POPULAR_VIDEO_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=viewCount&maxResults=1&type=video`;
+    // Eemaldasime '&videoDuration=medium' parameetri, et saada kätte kõik videod
+    const LATEST_VIDEOS_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=20&type=video`;
+    const POPULAR_VIDEO_URL = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=viewCount&maxResults=5&type=video`;
 
     try {
-        // Teeme mõlemad päringud korraga, et olla kiirem
         const [latestResponse, popularResponse] = await Promise.all([
             fetch(LATEST_VIDEOS_URL),
             fetch(POPULAR_VIDEO_URL)
@@ -20,20 +20,11 @@ exports.handler = async function(event, context) {
         const latestData = await latestResponse.json();
         const popularData = await popularResponse.json();
 
-        // Filtreerime mõlemast tulemusest välja "Shorts" videod
-        const filterShorts = (video) => {
-            const thumbnail = video.snippet.thumbnails.default;
-            return thumbnail.width >= thumbnail.height;
-        };
-
-        const latestVideos = latestData.items.filter(filterShorts);
-        const popularVideo = popularData.items.filter(filterShorts);
-
         return {
             statusCode: 200,
             body: JSON.stringify({
-                latestVideos: latestVideos,
-                popularVideo: popularVideo[0] // Saadame ainult ühe popima video objekti
+                latestVideos: latestData.items,
+                popularVideo: popularData.items[0]
             })
         };
     } catch (error) {
