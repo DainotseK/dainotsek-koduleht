50d850a65a0e07dc17d5d27f035fa7ba64069bba
DainotseK <dainotsek@gmail.com>
2025-07-03 22:53:10 +0300

    Update getAuthConfig.js

--- netlify/functions/getAuthConfig.js
+++ netlify/functions/getAuthConfig.js
@@ -1,9 +1,13 @@
 exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            domain: process.env.AUTH0_DOMAIN,
-            clientId: process.env.AUTH0_CLIENT_ID
-        })
-    };
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // või kitsamalt: "https://sinu-domeen.netlify.app"
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      domain: process.env.AUTH0_DOMAIN,
+      clientId: process.env.AUTH0_CLIENT_ID
+    })
+  };
 };
